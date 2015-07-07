af743e604ceca0847437934a143fd958f0633125
James Ginns <starvagrant@yahoo.com>
2015-07-07 14:37:45 -0500

    beginning hand project for superpac
    My new project, in cardhand.php is to pull a group of cards that
    would be in superpac into a hand full of cards. From there it's time
    to play them.
    
    A thing left undone in my other project, openrpg, is using the jquery
    plugin chosen to get better select menu buttons. Currently this is
    not functioning.
    
    Next step is to start modifying the css file I'm using as a template

---
+++ cardhand.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+	<meta charset="utf-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/cardhand.css" />
+</head>
+
+<body>
+    <div id="page">
+      <h2>Super Pac</h2>
+      <ul>
+	<li>Action Card</li>
+	<li>Primary</li>
+        <li class="hot">Self: Rule Explanation
+        <li id="four">Oppponent: Rule Explantion</li>
+      </ul>
+    </div>
+</body>
+</html>
