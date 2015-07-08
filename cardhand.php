733c70d5db6275ab1b41e2fd3a3fc6c5d2b21df1
James Ginns <starvagrant@yahoo.com>
2015-07-07 22:49:03 -0500

    Can Add Cards to Hand
    Included Functionality to Add Cards (unordered lists) to the games
    hand. Now I have to add some ajax so as to get random cards

--- cardhand.php
+++ cardhand.php
@@ -11,32 +11,34 @@
     <div id="page">
       <h2>Super Pac</h2>
       <ul id="first">
-	<li>Action Card</li>
+	<li class="card-type">Action Card</li>
 	<li class="card-name">Primary</li>
         <li class="self"><span class="title">Self:</span> <p>Rule Explanation</p>
         <li id="opponent"><span class="title">Opponent:</span> <p>Rule Explantion</p>
       </ul>
 
       <ul>
-	<li>Action Card</li>
+	<li class="card-type">Action Card</li>
 	<li class="card-name">Primary</li>
         <li class="self"><span class="title">Self:</span> <p>Rule Explanation</p>
         <li class="opponent"><span class="title">Opponent:</span> <p>Rule Explantion</p>
       </ul>
 
 	<ul>
-	<li>Money Card</li>
+	<li class="card-type">Money Card</li>
 	<li class="card-name">Big Bank Bailout</li>
         <li class="ethics">Dirty Money</li> 
         <li class="amount">$50</li> 
         <li class="quote">A big bank gets money, regardless of performance</li> 
       </ul>
+	<button> Draw a Card </button>
     </div>
 <script src="jquery-1.11.0.js"></script>
 <script>
 	var listItems = $('li');
 	var firstListItems = $('li:first-of-type');
 	var unOrderedLists = $('ul');
+	var button = $('button');
 
 	listItems.hide();
 	firstListItems.show();	
@@ -47,6 +49,14 @@
 	unOrderedLists.on('mouseout', function() {
 		$(this).children().hide();
 		firstListItems.show();	
+	});
+
+	button.on('click', function() {
+		console.log('clicked');
+		$(this).before("<ul><li>New List</ul></li>");
+		$(this).prev().on('mouseover', function(){
+			alert('hovering');
+		});
 	});
 </script>
 </body>
