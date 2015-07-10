4f7419354803e9aed2565b0dc41029655a8f644c
James Ginns <starvagrant@yahoo.com>
2015-07-09 21:22:24 -0500

    Began the UI for superpac in separte script
    There are a number of elements I'll need to get Super Pac up and
    running. One is a user interface, which I've been working on.
    Currently the program has no internal logic, it just displays
    three different card types.

--- cardhand.php
+++ cardhand.php
@@ -3,30 +3,41 @@
 
 <head>
 	<meta charset="utf-8">
-<title></title>
+<title>Super Pac</title>
 <link rel="stylesheet" type="text/css" href="css/cardhand.css" />
 </head>
 
 <body>
-    <div id="page">
+	<div id="candidate-page">
+		<ul>
+			<li class="cardtype">Candidate</li>
+			<li><ul>
+				<li>Stats</li>
+				<li>Stats</li>
+				<li>Stats</li>
+				<li>Stats</li>
+			</li></ul>
+	</div>
+	</div>
+	<div id="page">
       <h2>Super Pac</h2>
       <ul id="first">
-	<li class="card-type">Action Card</li>
-	<li class="card-name">Primary</li>
+	<li class="cardtype">Action Card</li>
+	<li class="name">Primary</li>
         <li class="self"><span class="title">Self:</span> <p>Rule Explanation</p>
-        <li id="opponent"><span class="title">Opponent:</span> <p>Rule Explantion</p>
+        <li class="opponent"><span class="title">Opponent:</span> <p>Rule Explantion</p>
       </ul>
 
       <ul>
-	<li class="card-type">Action Card</li>
-	<li class="card-name">Primary</li>
+	<li class="cardtype">Action Card</li>
+	<li class="name">Primary</li>
         <li class="self"><span class="title">Self:</span> <p>Rule Explanation</p>
         <li class="opponent"><span class="title">Opponent:</span> <p>Rule Explantion</p>
       </ul>
 
 	<ul>
-	<li class="card-type">Money Card</li>
-	<li class="card-name">Big Bank Bailout</li>
+	<li class="cardtype">Money Card</li>
+	<li class="name">Big Bank Bailout</li>
         <li class="amount">$50</li> 
         <li class="quote">A big bank gets money, regardless of performance</li> 
       </ul>
@@ -34,51 +45,6 @@
 	<button id="action"> Draw an Action Card </button>
     </div>
 <script src="jquery-1.11.0.js"></script>
-<script>
-	var listItems = $('li');
-	var firstListItems = $('li:first-of-type');
-	var unOrderedLists = $('ul');
-	var button = $('button');
-	var request;
-	var list;
-
-	listItems.hide();
-	firstListItems.show();	
-
-	unOrderedLists.on('mouseover', function() {
-		$(this).children().show();
-	});
-	unOrderedLists.on('mouseout', function() {
-		$(this).children().hide();
-		firstListItems.show();	
-	});
-
-	button.on('click', function(event) {
-		var thisButton = $(this);
-		var cardType = event.target.id;
-		var data = "cards." + cardType + ".json"
-		request = $.get(data, function() {
-			console.log("success");
-		})
-		.done(function(data){
-			// generate a random card
-			var card = Math.floor(Math.random() * data.length);
-			console.log(card);
-			list = '<ul>\n'
-			for (fields in data[card]){
-				list += '<li class="' + fields + '">' +	data[card][fields] + '</li>\n';
-			}
-			list += '</ul>';
-
-			$(list).insertAfter('ul:last');
-		})
-		.fail(function(){
-//			console.log('no data');
-		})
-		.always(function(){
-//			console.log('finished');
-		}); // end request
-	}); // end on function
-</script>
+<script src="js/superpacui.js"></script>
 </body>
 </html>
---
+++ js/superpacui.js
@@ -0,0 +1,46 @@
+var button = $('button');
+
+function makeMenus(){ 	// makes every non-first list item disappear
+
+	var listItems = $('#page li').not('.cardtype');
+	var firstListItems = $('#page li.cardtype');
+	var unOrderedLists = $('#page ul');
+	listItems.hide();
+	firstListItems.show();
+	unOrderedLists.on('mouseover', function() {
+		$(this).children().show();
+	});
+	unOrderedLists.on('mouseout', function() {
+		$(this).children().not('.cardtype').hide();
+		firstListItems.show();	
+	});
+}
+	
+	makeMenus();
+	
+	button.on('click', function(event) {
+		var thisButton = $(this);
+		var cardType = event.target.id;
+		var data = "json/cards." + cardType + ".json"
+		var request = $.get(data, function() {
+			console.log("success");
+		})
+		.done(function(data){
+			// generate a random card
+			var card = Math.floor(Math.random() * data.length);
+			var list = '<ul>\n'
+			for (fields in data[card]){
+				list += '<li class="' + fields + '">' +	data[card][fields] + '</li>\n';
+			}
+			list += '</ul>';
+
+			$(list).insertAfter('#page ul:last');
+			makeMenus();
+		})
+		.fail(function(){
+//			console.log('no data');
+		})
+		.always(function(){
+//			console.log('finished');
+		}); // end request
+	}); // end on function
