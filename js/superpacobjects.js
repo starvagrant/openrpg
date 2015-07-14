b5fa0a91ea010a5e02d762ae2132ca4366ca792a
James Ginns <starvagrant@yahoo.com>
2015-07-14 15:02:12 -0500

    Superpac objects draws random card from json file
    Previously, in working with superpacobjects.js, the cards loaded
    and constructed were based on test values. Now they are random
    cards from the json file.
    
    Also, the json file has been restructured so that all cards have
    cardtypes and card names. This should simplify working with different
    types of cards. Next step is to make the UI function click draw call
    my hand.prototype.draw function

--- js/superpacobjects.js
+++ js/superpacobjects.js
@@ -10,7 +10,7 @@ What are my data structures?
 
 function ActionCard(cardJson){			// constructor
 
-	this.purpose = "action";
+	this.cardJson = cardJson;
 	this.cardHTML = "<ul>\n"
 		for(field in cardJson){
 			this.cardHTML += '<li class="' + field + '>' + cardJson[field] + '</li>\n';
@@ -18,11 +18,12 @@ function ActionCard(cardJson){			// constructor
 	this.cardHTML += "</ul>\n";
 	console.log(this.cardHTML);
 
-	// 	cardtype, name, self_rules, opponent_rules
+// json fields:
+// cardtype, name, self_rules, opponent_rules
 }
 function MoneyCard(cardJson){			// constructor
 
-	this.purpose = "money";
+	this.cardJson = cardJson;
 	this.cardHTML = "<ul>\n"
 		for(field in cardJson){
 			this.cardHTML += '<li class="' + field + '>' + cardJson[field] + '</li>\n';
@@ -30,32 +31,33 @@ function MoneyCard(cardJson){			// constructor
 	this.cardHTML += "</ul>\n";
 	console.log(this.cardHTML);
 
-//	cardtype, amount, name, quote
+// json fields: 
+// cardtype, amount, name, quote
 }
 function CandidateCard(cardJson){			// constructor
 
-	this.purpose = "candidate";
-
+	this.cardJson = cardJson;
 	this.cardHTML = "<ul>\n";
-	this.cardHTML += '<li class="cardtype">candidate</li>\n';
-	this.cardHTML += '<li class="candidate">' + cardJson.candidate + '</li>';
+	this.cardHTML += '<li class="cardtype">' + cardJson.cardtype + '</li>\n';
+	this.cardHTML += '<li class="name">' + cardJson.name + '</li>\n';
 	for (field in cardJson.stats){
 		this.cardHTML += '<li class="' + field + '>' + cardJson.stats[field] + '</li>\n';
 	}
 	this.cardHTML += '</ul>';
 	console.log(this.cardHTML);
 
-//	candidate, stats object (military, economic stability, infrastructure, nationalism, energy_infrastructure)
+// json fields: 
+// card type, candidate, stats object (military, economic stability, infrastructure, nationalism, energy_intensity)
 }
 
 /* Card is a Factory */
-function Card (cardType, cardName, cardJson){			// constructor
+function Card (cardJson){			// constructor
+//	console.log(cardJson);
 
-	this.cardName = cardName;
-	this.cardJson = cardJson;
-	if (cardType == 'money') this.traits = new MoneyCard(cardJson);
-	if (cardType == 'action') this.traits = new ActionCard(cardJson);
-	if (cardType == 'candidate') this.traits = new CandidateCard(cardJson);
+	this.cardName = cardJson.name
+	if (cardJson.cardtype == 'money') this.traits = new MoneyCard(cardJson);
+	if (cardJson.cardtype == 'action') this.traits = new ActionCard(cardJson);
+	if (cardJson.cardtype == 'candidate') this.traits = new CandidateCard(cardJson);
 }
 
 function Hand(type){			// constructor
@@ -69,32 +71,29 @@ function Hand(type){			// constructor
 
 /* Hand.prototype.draw will be called by a button 'onclick' event */
 
-Hand.prototype.draw = function(type, name, json){			// prototype
-//	console.log(name);
-//	console.log(type);
-	var newCard = new Card(type, name, json);
+Hand.prototype.draw = function(json){			// prototype
+
+	var newCard = new Card(json);
 	this.cards.push(newCard);
-//	console.log(this.cards);
+	console.log(this.cards);
 };
 
 ////////////////// TESTS ////////////////////////
 var playerHand = new Hand('player');
-var blankobject = {};
+
+var cardsInArray = playerCardArray.length;
+var randomCardNumber = Math.floor(Math.random() * cardsInArray);
+var testCard = playerCardArray[randomCardNumber];
+
+console.log(testCard);
+playerHand.draw(testCard);
+
 /*
-var moneyTest = {"cardtype": "money", "amount": 10, "name": "large donor", "quote": "well financed"};
-playerHand.draw('money', '5', moneyTest);
-var actionTest = {"cardtype": "action", "name": "Primary", "self_rules" : "self", "opponent_rules" : "opponent"};
-playerHand.draw('action', 'Primary', actionTest);
-*/
 var candidateHand = new Hand('candidate');
-var candidateTest = { 
-	"candidate" : "Lindsey Grime",
-	"stats" : {
-		"military": "-3",
-		"economic_stability": "1",
-		"infrastructure": "3",
-		"nationalism": "-1",
-		"energy_intensity": "4" }
-		}
-candidateHand.draw('candidate', 'Squiggles Magoo', candidateTest);
-console.log('yoyoyo? finsihed');
+var cardsInArray = candidateCardArray.length;
+var randomCardNumber = Math.floor(Math.random() * cardsInArray);
+
+// testCard is a json object
+var testCard = candidateCardArray[randomCardNumber];
+candidateHand.draw(testCard);
+*/
